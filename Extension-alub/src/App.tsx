942b302fb6a710f0028acef5ebfd036eb3240836
Wkjung <wkjung0329@gmail.com>
2021-11-02 19:52:28 +0900

    commit 버튼 자동생성 및 commit버튼 누를시 자동으로 소스코드 페이지로 이동 및 필요 정보 복사

--- Extension-alub/src/App.tsx
+++ Extension-alub/src/App.tsx
@@ -1,25 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ChromeMessage, Sender, getCurrentTabUId, getCurrentTabUrl} from './types';
 import './App.css'
 
+import { Form, Button} from 'react-bootstrap';
+
+import { useForm, Controller } from 'react-hook-form';
 const App = () => {
     const [url, setUrl] = useState('')
     const [responseFromContent, setResponseFromContent] = useState('')
-    const [bojId, setBojId] = useState('')
+    const {
+        trigger,
+        watch,
+        handleSubmit,
+        control,
+      } = useForm();
+    const hours = useRef()
+    const minutes = useRef()
+    const seconds = useRef()
+    const [config, setConfig] = useState("DEFAULT")
+
     useEffect(() => {
         getCurrentTabUrl((url) => {
             setUrl(url || "undefined")
         })
-        getBojInfo()
-        addStatusTable()
     },[])
 
+    
     const sendTestMessage = () => {
         const message: ChromeMessage = {
             from: Sender.React,
             message: "Hello from React",
         }
-
         getCurrentTabUId((id) => {
             id && chrome.tabs.sendMessage(
                 id,
@@ -31,22 +42,6 @@ const App = () => {
     };
 
     
-    const sendCopyMessage = () => {
-        const message: ChromeMessage = {
-            from: Sender.React,
-            message: "copy",
-        }
-
-        getCurrentTabUId((id) => {
-            id && chrome.tabs.sendMessage(
-                id,
-                message,
-                (responseFromContentScript) => {
-                    setResponseFromContent(responseFromContentScript);
-                });
-        });
-    };
-
     const sendRemoveMessage = () => {
         const message: ChromeMessage = {
             from: Sender.React,
@@ -62,25 +57,14 @@ const App = () => {
                 });
         });
     };
-    const getBojInfo = () => {
-        const message: ChromeMessage = {
-            from: Sender.React,
-            message: "get boj info",
-        }
 
-        getCurrentTabUId((id) => {
-            id && chrome.tabs.sendMessage(
-                id,
-                message,
-                (response) => {
-                    setBojId(response);
-                });
-        });
-    }
-    const addStatusTable = () => {
+    const setTimer = () => {
+        hours.current = watch("hours", "")
+        minutes.current = watch("minutes", "")
+        seconds.current = watch("seconds", "")
         const message: ChromeMessage = {
             from: Sender.React,
-            message: "add status table",
+            message: {data:{hh: hours, mm: minutes, ss:seconds}, message:"setTimer"},
         }
 
         getCurrentTabUId((id) => {
@@ -92,24 +76,84 @@ const App = () => {
                 });
         });
     }
+
     return (
         <div className="App">
             
                 <div>
                     <p>URL:</p>
                     <p>{url}</p>
-                    <p>백준 ID:</p>
-                    <p>{bojId}</p>
+
                 </div>
                 <button onClick={sendTestMessage}> Send Message from chrome</button>
                 <button onClick={sendRemoveMessage}> Delete logo</button>
-                <button onClick={getBojInfo}> getinfo</button>
-                <button style={{color : 'white', backgroundColor: "black"}} onClick={sendCopyMessage}>copy the code</button>
-                
+
+                <div>
+                    <Form onSubmit={handleSubmit(setTimer)}>
+                        <Form.Group>
+                            <Controller
+                                name="hours"
+                                control = {control}
+                                rules = {{
+                                    pattern: {
+                                        value: /^(?=.*\d)/,
+                                        message: "숫자를 적어주세요"
+                                    }
+                                }}
+                                defaultValue = "0"
+                                render={({ field }) => (
+                                    <Form.Control
+                                      placeholder="시"
+                                      {...field}
+                                    />)}
+                            />
+                            <span>:</span>
+                            <span>
+                            <Controller
+                                name="minutes"
+                                control = {control}
+                                rules = {{
+                                    pattern: {
+                                        value: /^(?=[0-59])/,
+                                        message: "0-59사이의 숫자를 적어주세요"
+                                    }}}
+                                defaultValue= "0"
+                                render={({ field }) => (
+                                    <Form.Control
+                                      placeholder="분"
+                                      {...field}
+                                    />)}
+                            />
+                            </span>
+                            <span>:</span>
+                            <span>
+                            <Controller
+                                name="seconds"
+                                control = {control}
+                                rules = {{
+                                    pattern: {
+                                        value: /^(?=[0-59])/,
+                                        message: "0-59사이의 숫자를 적어주세요"
+                                    }}}
+                                defaultValue= "0"
+                                render={({ field }) => (
+                                    <Form.Control
+                                      placeholder="초"
+                                      {...field}
+                                    />)}
+                            />
+                            </span>
+                        </Form.Group>
+                        <div>
+                            <Button type="submit">타이머 설정하기</Button>
+                        </div>
+                    </Form>  
+                </div>
                 <p>Response from content:</p>
                 <p>
                     {responseFromContent}
                 </p>
+                <p>{config}</p>
         </div>
     );
 };
